d1219209e329cf690e1d2053ac31350027951745
bseym <br.grego@gmail.com>
2019-12-05 16:57:03 +0100

    add game card

---
+++ src/Card.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './Card.css'
+
+const HIDDEN_SYMBOL = '❓'
+
+const Card = ({ card, feedback, index, onClick }) => ( 
+    <div className= {`card ${feedback}`} onClick={() => onClick(index)}>
+
+        <span className= "symbole">
+            {feedback === 'hidden' ? HIDDEN_SYMBOL : card}    
+        </span> 
+    </div>
+)
+    Card.propTypes = {
+        card: PropTypes.string.isRequired,
+        feedback: PropTypes.oneOf([
+            'visible',
+            'hidden',
+            'justMatched',
+            'justMismatched',
+        ]).isRequired, 
+        index: PropTypes.number.isRequired,
+        onClick: PropTypes.func.isRequired,
+
+    }
+
+export default Card
